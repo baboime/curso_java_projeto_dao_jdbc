ed5b9d199d78927bab52dd072727bd65dc4f939b
Edney Baboim <ednney@gmail.com>
2020-05-07 16:26:11 -0300

    Criação das Interfaces DAO para Vendedor e Departamento

---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Vendedor;
+
+public interface VendedorDao {
+	
+	void inserir(Vendedor obj);
+	void atualizar(Vendedor obj);
+	void excluirPeloId(Integer id);
+	Vendedor buscarPeloId(Integer id);
+	List<Vendedor> buscarTudo();
+
+}
