e872c2776ef4043e9273844aa431d860daddc40c
Edney Baboim <ednney@gmail.com>
2020-05-07 16:00:42 -0300

    Criação do obejto departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+import model.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Teste");
+		
+		System.out.println(obj);
+		
+
+	}
+
+}
---
+++ src/model/entidades/Vendedor.java
@@ -0,0 +1,11 @@
+package model.entidades;
+
+public class Vendedor {
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private Date dataDeNascimento;
+	
+
+}
