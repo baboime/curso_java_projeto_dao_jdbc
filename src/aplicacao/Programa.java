df0cb42356c461b6ab4a32bbb5172aa267b4d890
Edney Baboim <ednney@gmail.com>
2020-05-07 16:36:43 -0300

    Criação do DaoFactory

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,6 +3,8 @@ package aplicacao;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entidades.Departamento;
 import model.entidades.Vendedor;
 
@@ -15,6 +17,8 @@ public class Programa {
 		Departamento obj = new Departamento(1, "Teste");
 		
 		Vendedor vendedor = new Vendedor(1, "Edney", "edney.baboim.gmail.com", sdf.parse("16/07/1981"), 9000.0, obj);
+		
+		VendedorDao vendedorDao = DaoFactory.criarVendedorDao();
 		
 		System.out.println(obj);
 		System.out.println(vendedor);
---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,42 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.VendedorDao;
+import model.entidades.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao{
+
+	@Override
+	public void inserir(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluirPeloId(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor buscarPeloId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> buscarTudo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
